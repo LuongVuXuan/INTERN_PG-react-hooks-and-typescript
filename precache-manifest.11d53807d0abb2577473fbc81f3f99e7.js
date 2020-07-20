a702f8dfb6c8282faa14e9b2a93220ce69915b3c
LuongDzai <vuxuanluong12345@gmail.com>
2020-07-20 16:56:14 +0700

    Updates

---
+++ precache-manifest.11d53807d0abb2577473fbc81f3f99e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6423f4a3b7a82bb4b5709888cf301033",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "ba0a31e09eeb1ed944a0",
+    "url": "/my-react-app/static/css/main.3669c503.chunk.css"
+  },
+  {
+    "revision": "65c715fcd2af5c3614f4",
+    "url": "/my-react-app/static/js/2.d2072b26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-react-app/static/js/2.d2072b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0a31e09eeb1ed944a0",
+    "url": "/my-react-app/static/js/main.8f4c6a40.chunk.js"
+  },
+  {
+    "revision": "435064591b6543e8ca17",
+    "url": "/my-react-app/static/js/runtime-main.c0584337.js"
+  }
+]);
